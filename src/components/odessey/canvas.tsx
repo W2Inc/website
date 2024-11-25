f961d0eae4abf6e5d84fdef1969976b857f9c26f
W2Wizard <main@w2wizard.dev>
2024-11-25 16:30:02 +0100

    Update homepage again

--- src/components/odessey/canvas.tsx
+++ src/components/odessey/canvas.tsx
@@ -4,103 +4,90 @@ import { useTexture } from '@react-three/drei';
 import { MeshBasicMaterial, MirroredRepeatWrapping, PlaneGeometry, Texture } from 'three';
 import { DEG2RAD } from 'three/src/math/MathUtils.js';
 
-const MyComponent = ({  }) => {
-  const warpTexture = useTexture("./odessey.jpg");
-  const [clonedTexture, setClonedTexture] = useState<Texture | undefined>(undefined);
+const Odessey = () => {
+    const warpTexture = useTexture("./odessey.jpg");
+    const [clonedTexture, setClonedTexture] = useState<Texture | undefined>(undefined);
 
-  warpTexture.colorSpace = 'srgb';
-  warpTexture.wrapS = MirroredRepeatWrapping;
-  warpTexture.wrapT = MirroredRepeatWrapping;
-  useEffect(() => {
+    warpTexture.colorSpace = 'srgb';
+    warpTexture.wrapS = MirroredRepeatWrapping;
+    warpTexture.wrapT = MirroredRepeatWrapping;
+    useEffect(() => {
+        const cloned = warpTexture.clone();
+        cloned.colorSpace = 'srgb';
+        cloned.wrapS = MirroredRepeatWrapping;
+        cloned.wrapT = MirroredRepeatWrapping;
+        setClonedTexture(cloned);
+    }, [warpTexture]);
 
-      const cloned = warpTexture.clone();
-      cloned.colorSpace = 'srgb';
-      cloned.wrapS = MirroredRepeatWrapping;
-      cloned.wrapT = MirroredRepeatWrapping;
-      setClonedTexture(cloned);
-  }, [warpTexture]);
 
-  const getWarpTexture = () => warpTexture;
-  const getClonedTexture = () => clonedTexture;
-
-  useFrame((_, delta) => {
-    const speed = 0.06;
-    const wt = getWarpTexture();
-    if (wt) wt.offset.x += speed * delta;
-    const ct = getClonedTexture();
-    if (ct) ct.offset.x += speed * delta;
-  });
-
-  const heightIterations = 5;
-  const height = 40;
-  const offset = 0.2;
-  const gap = 4.5;
-  const rotationInDegrees = 85;
-
-  const MyGeometry = useMemo(() => {
-    const geom = new PlaneGeometry(100, height);
-    if (geom.attributes.uv) {
-      geom.attributes.uv.array[2] = 0.125;
-      geom.attributes.uv.array[6] = 0.125;
-      geom.attributes.uv.needsUpdate = true;
-    }
-    return geom;
-  }, [height]);
-
-  const RightMaterial = useMemo(() => {
-    return new MeshBasicMaterial({
-        map: getWarpTexture(),
-        transparent: true
+    useFrame((_, delta) => {
+        const speed = 0.06;
+        if (warpTexture) warpTexture.offset.x += speed * delta;
+        if (clonedTexture) clonedTexture.offset.x += speed * delta;
     });
-  }, [getWarpTexture]);
 
-    const LeftMaterial = useMemo(() => {
+    const heightIterations = 5;
+    const height = 40;
+    const offset = 0.2;
+    const gap = 4.5;
+    const rotationInDegrees = 85;
+
+    const geometry = useMemo(() => {
+        const geom = new PlaneGeometry(100, height);
+        if (geom.attributes.uv) {
+          geom.attributes.uv.array[2] = 0.125;
+          geom.attributes.uv.array[6] = 0.125;
+          geom.attributes.uv.needsUpdate = true;
+        }
+        return geom;
+      }, [height]);
+
+    const rightMaterial = useMemo(() => {
         return new MeshBasicMaterial({
-            map: getClonedTexture(),
+            map: warpTexture,
             transparent: true
         });
-    }, [getClonedTexture]);
+      }, [warpTexture]);
 
-  if (!warpTexture || !clonedTexture) {
-    return null;
-  }
+        const leftMaterial = useMemo(() => {
+            return new MeshBasicMaterial({
+                map: clonedTexture,
+                transparent: true
+            });
+        }, [clonedTexture]);
+
+  if (!warpTexture || !clonedTexture) return null;
+
+  const renderMeshes = (isLeft: boolean) =>
+    Array.from({ length: heightIterations }).map((_, i) => {
+      const isOdd = i % 2 === 0;
+      const positionX = isLeft ? -gap + offset : gap - offset;
+      const rotationY = isLeft ? rotationInDegrees * DEG2RAD : -rotationInDegrees * DEG2RAD;
+      const material = isLeft ? leftMaterial : rightMaterial;
+
+      return (
+        <mesh
+          key={`${isLeft ? 'left' : 'right'}-${i}`}
+          position={[positionX, height * i, 0]}
+          rotation-y={rotationY}
+          scale-y={isOdd ? -1 : 1}
+          scale-x={isLeft ? 1 : -1}
+        >
+          <primitive object={geometry} />
+          <primitive object={material} />
+        </mesh>
+      );
+    });
 
   return (
     <>
-        <color attach="background" args={['black']} />
-        <group>
-          {Array.from({ length: heightIterations }).map((_, i) => {
-            const isOdd = i % 2 === 0;
-            return (
-              <mesh
-                key={`left-${i}`}
-                position={[-gap + offset, height * i, 0]}
-                rotation-y={rotationInDegrees * DEG2RAD}
-                scale-y={isOdd ? -1 : 1}
-              >
-                <primitive object={MyGeometry} />
-                <primitive object={LeftMaterial} opacity={1} />
-              </mesh>
-            );
-          })}
-          {Array.from({ length: heightIterations }).map((_, i) => {
-            const isOdd = i % 2 === 0;
-            return (
-              <mesh
-                key={`right-${i}`}
-                position={[gap - offset, height * i, 0]}
-                rotation-y={-rotationInDegrees * DEG2RAD}
-                scale-y={isOdd ? -1 : 1}
-                scale-x={-1}
-              >
-                <primitive object={MyGeometry} />
-                <primitive object={RightMaterial} opacity={1} />
-              </mesh>
-            );
-          })}
-        </group>
+      <color attach="background" args={['black']} />
+      <group>
+        {renderMeshes(true)}
+        {renderMeshes(false)}
+      </group>
     </>
   );
 };
 
-export default MyComponent;
+export default Odessey;
--- src/components/odessey/index.tsx
+++ src/components/odessey/index.tsx
@@ -1,12 +1,10 @@
 import { Canvas } from "@react-three/fiber";
-import MyComponent from "./canvas";
+import Odessey from "./canvas";
 import Post from "./post";
 
 export default () => (
-    <>
-        <Canvas className="d3" style={{ zIndex: 1 }} gl={{ localClippingEnabled: true }}>
-            <MyComponent></MyComponent>
-            <Post />
-        </Canvas>
-    </>
+    <Canvas className="d3" style={{ zIndex: 1 }}>
+        <Odessey />
+        <Post />
+    </Canvas>
 );
